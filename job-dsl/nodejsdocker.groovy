3087df14083a65dd9d3c57ca4c4d960c6a4fdb9e
keyman kim <keyman91@gmail.com>
2026-04-27 14:50:01 +0900

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('keyman91/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockerhub-keyman')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
